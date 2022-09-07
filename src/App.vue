6094ef3e8c8646aa747514bb590678a0310be2af
Smurrowz <nigeldn1995@gmail.com>
2022-09-07 15:19:59 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainHeader></MainHeader>
+    <MainContent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue';
+import MainContent from './components/MainContent.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainHeader,
+    MainContent
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './var.scss';
+  @import './index.scss';
+  #app{
+    background-color: $bg-darkblue;
+  }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <img src="../assets/img/Spotify.svg" alt="logo spotify">
+  </header>
+</template>
+<script>
+  export default {
+    name: 'MainHeader'
+  }
+
+</script>
+<style scoped lang="scss">
+  @import '../var.scss';
+  header{
+    background-color: $bg-blue;
+    padding: 10px 20px;
+    & img{
+      height: 50px
+    }
+  }
+</style>
