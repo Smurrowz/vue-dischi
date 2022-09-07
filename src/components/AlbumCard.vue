c6fca484e734a020378aab11bef9d33525284c83
Smurrowz <nigeldn1995@gmail.com>
2022-09-07 15:36:21 +0200

    refactoring done

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row gap-4">
+    <div class="my-card" v-for="(album, index) in albums" :key="index" style="width: 13rem;">
+      <img class="my-card-img" :src="album.poster" :alt="album.title">
+      <div class="my-card-bottom">
+        <h5>{{album.title}}</h5>
+        <p class="my-card-description">
+          {{album.author}} <br>
+          {{album.year}}
+        </p>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'AlbumCard',
+  props: {
+    albums: Array,
+  },
+  
+}
+</script>
+<style scoped lang="scss">
+@import '../var.scss';
+@import '../index.scss';
+
+main {
+  padding: 70px 0;
+}
+
+.my-card {
+  background-color: $bg-blue;
+  padding: 20px;
+
+  & .my-card-img {
+    width: 100%;
+  }
+
+  & .my-card-bottom {
+    text-align: center;
+
+    & h5 {
+      color: white;
+      text-transform: uppercase;
+      padding: 20px 0;
+    }
+
+    & .my-card-description {
+      color: grey;
+      font-size: 20px;
+      font-weight: 500;
+    }
+  }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,36 +1,30 @@
 <template>
   <main>
     <div class="my-container">
-      <div class="row gap-4" >
-        <div class="my-card"  v-for="(album, index) in listAlbums" :key="index" style="width: 13rem;">
-          <img class="my-card-img" :src="album.poster" :alt="album.title" >
-          <div class="my-card-bottom">
-            <h5>{{album.title}}</h5>
-            <p class="my-card-description">
-              {{album.author}} <br>
-              {{album.year}}
-            </p>
-          </div>
-        </div>
-      </div>
+
+      <AlbumCard :albums="listAlbums" />
+
     </div>
   </main>
 </template>
 <script>
-  import axios from "axios";
-  export default {
-  name: 'MainContent',
-  data(){
-    return{
+import AlbumCard from './AlbumCard.vue';
+import axios from "axios";
+
+export default {
+  name: "MainContent",
+  components: { AlbumCard },
+  data() {
+    return {
       listAlbums: [],
     }
   },
-  created(){
+  created() {
     axios
-    .get('https://flynn.boolean.careers/exercises/api/array/music')
-    .then((res) =>{
-      this.listAlbums = res.data.response;
-    })
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((res) => {
+        this.listAlbums = res.data.response;
+      })
   }
 }
 </script>
@@ -40,25 +34,5 @@
 
 main {
   padding: 70px 0;
-}
-.my-card{
-  background-color: $bg-blue;
-  padding: 20px;
-  & .my-card-img{
-    width: 100%;
-  }
-  & .my-card-bottom{
-    text-align: center;
-    & h5{
-      color: white;
-      text-transform: uppercase;
-      padding: 20px 0;
-    }
-    & .my-card-description{
-      color: grey;
-      font-size: 20px;
-      font-weight: 500;
-    }
-  }
 }
 </style>
